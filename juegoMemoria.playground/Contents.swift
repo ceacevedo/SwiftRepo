ad1ec975bae178d8876d60519719b974f804aacd
Carlos Acevedo <carlosacevedo@macbook-air-de-carlos.local>
2016-08-22 20:46:08 -0500

    Creación del proyecto
    Se crea el proyecto y se sube el playground con la solución de la tarea

---
+++ juegoMemoria.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let numeros = 1...101
+
+for numero in numeros{
+    if (numero % 5) == 0 {
+        print("# \(numero) Bingo!!!")
+    }
+    if (numero % 2) == 0 {
+        print("# \(numero) par!!!")
+    }else {
+        print("# \(numero) impar!!!")
+    }
+    if numero >= 30 && numero <= 40 {
+        print("# \(numero) Viva Swift!!!")
+    }
+}
